135785fdae45f34cd4bd304bf64696235dcbfd2e
crittenbach1 <crittenbach@gmail.com>
2019-03-01 08:09:26 -0500

    recipe schema has a nested stepSchema array

--- models/recipe.js
+++ models/recipe.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
+let stepSchema = new Schema({
+   description: {
+     type: String,
+     required: true
+   }
+});
 
 let recipeSchema = new Schema;
 
     recipeSchema.add({
+      steps: [stepSchema],
       name: {
         type: String,
         required: true
       }
     });
-
-
 
 module.exports = mongoose.model('Recipe', recipeSchema);
